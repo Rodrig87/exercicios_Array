600125adad6818941f9c08a0ae678e8e291c1185
Rodrigo Azevedo <134436718+Rodrig87@users.noreply.github.com>
2023-10-10 11:51:04 -0300

    Update exercicio-19.py

--- exercicio-19.py
+++ exercicio-19.py
@@ -1,9 +1,11 @@
-a = []
-numimpar = 1
+a = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 contador = 0
-while contador < 10:
-    a.append(numimpar)
-    numimpar = numimpar + 2
-    contador = contador + 1
-
+impar = 0
+while True:
+    if contador%2 != 0:
+        a[impar] = contador
+        impar = impar + 1
+    contador += 1
+    if impar >= 10:
+        break
 print(a)
